ee49b62c4034cf65f87798a9fe1f940759c72e38
CarrishCopilot <CarrishCopilot@163.com>
2026-04-17 13:45:50 +0800

    Set base URL and output directory in Vite config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
   ],
+  base: '/HalfPaper/',
+  build: {
+    outDir: 'dist'
+  }
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
